153cb3ef25b81cfaaa9be03177bb2a50a1196217
Rodrigo Daniel Euan Briceño <rodrigo981913@gmail.com>
2024-10-31 17:58:01 -0600

    Day 1 Course C++

--- Slash/Source/Slash/Private/Item.cpp
+++ Slash/Source/Slash/Private/Item.cpp
@@ -3,7 +3,6 @@
 
 #include "Item.h"
 
-// Sets default values
 AItem::AItem()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
@@ -11,14 +10,18 @@ AItem::AItem()
 
 }
 
-// Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	UE_LOG(LogTemp, Warning, TEXT("Begin Play!"));
+	
+	if (GEngine) 
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
+	}
 }
 
-// Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
